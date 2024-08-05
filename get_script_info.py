71a1410adee1b13959ba978c16e29eddb2cd5aaf
Kevin Rush <kevinrush.net@gmail.com>
2024-08-05 12:25:09 +0200

    added script interpretation prompt

--- get_script_info.py
+++ get_script_info.py
@@ -6,7 +6,6 @@ import fitz  # PyMuPDF
 import re
 import json
 from screenplay_pdf_to_json import convert
-
 import base64
 from colorama import Fore
 import requests
@@ -61,6 +60,15 @@ def delete_image_dir(img_dir):
     except Exception as e:
         print(f"An error occurred while deleting {img_dir}: {e}")
 
+def delete_image(img_path):
+    # Delete the image at the given path
+    if os.path.exists(img_path):
+        os.remove(img_path)
+        print(f"Image file {img_path} deleted successfully.")
+    else:
+        print(f"Image file {img_path} does not exist.")
+
+
 # Function to encode the image
 def encode_image(image_path):
     #This function encodes the image to base64
@@ -69,7 +77,60 @@ def encode_image(image_path):
 
 def get_script_json_via_gpt(image_path):
     # This function takes an image and returns the alt text for the image
-    prompt = "ENTER_PROMPT"
+    prompt = """You are an expert script JSON converter. Your job is to take in an image of a single page of a script and convert the text on the page into JSON format. Note, you NEVER add text that was not originally in the script. All text must be traceable to the original script! If information is unknown because it is missing the value is left blank (Example: A page is only dialogue and does not list the act number or title.)
+     
+    The JSON file should contain the following keys: title, author, act[act_number, act_title], page_num, scenes [scene_number, location, extra_info, dialogues[character, dialogue, direction], intercut].
+
+    For example:
+    
+    {
+    "title": "Movie 1",
+    "author": "John Doe",
+    "act": {
+        "name": "ACT ONE",
+        "title": "Title 1"
+    },
+    "page_num": "1",
+    "scenes": [
+        {
+        "scene_number": 1,
+        "location": "EXT. LOCATION NAME - Lorem ipsum - FLASHBACK",
+        "extra_information": "a description of the scene"
+        },
+        {
+        "scene_number": 2,
+        "location": "EXT. LOCATION NAME - Lorem ipsum - PRESENT DAY",
+        "extra_information": "information about the scene and the background",
+        "dialogues": [
+            {
+            "character": "CHARACTER 1",
+            "dialogue": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            "direction": ""
+            },
+            {
+            "character": "CHARACTER 2",
+            "dialogue": "Duis tristique nibh vel tellus fermentum dictum."
+            "direction": "CHARACTER 2 reacts to CHARACTER 1"
+            }
+            ]
+            {
+        "intercut": "Information about what is happening at this point of the scene",
+        "dialogue": [
+            {
+            "character": "CHARACTER 1",
+            "dialogue": "Nullam vitae malesuada orci.",
+            "direction": "CHARACTER 3 does something as they enter the scene"
+            },
+            {
+            "character": "CHARACTER 3",
+            "dialogue": "Pellentesque habitant morbi tristique senectus et",
+            "direction": ""
+            },
+        ]
+        }
+    ]
+    }
+    """
 
     base64_image = encode_image(image_path)
     print(f"{Fore.YELLOW}\n-----------------Calling OpenAI-----------------{Fore.RESET}")
@@ -124,3 +185,22 @@ pdf_path = f'scripts/{script_name}.pdf'
 txt_path = f'scripts/{script_name}.txt'  
 img_path = f'scripts/{script_name}_images'
 
+
+
+# Testing
+
+exit()
+
+fp =  open(pdf_path, 'rb')
+scriptJSON = convert(fp, 0)
+
+# save the json to a file
+json_file = f'scripts/{script_name}.json'
+with open(json_file, 'w') as f:
+    json.dump(scriptJSON, f)
+
+# pdf_to_text(pdf_path, txt_path)
+
+pdf_to_images(pdf_path, img_path)
+
+delete_image_dir(img_path)
